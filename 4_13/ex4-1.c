429523ae81f379fd5611cdf5b98bc22eef678374
David Gustafson <git@radicalskeptic.net>
2014-06-14 18:11:31 -0700

    first commit

---
+++ 4_13/ex4-1.c
@@ -0,0 +1,10 @@
+/* 4_13/ex4-1
+ * write a program to print name, soc and dob
+*/
+
+#include <stdio.h>
+
+
+int main() {
+  printf("%s, SOC %d, DOB: %d-%d-%d", "David Gustafson", 567179224,6,28,72);
+}
