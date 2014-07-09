ee9ad606b91f3215c6d08d74bab1d01ade80fd52
David Gustafson <git@radicalskeptic.net>
2014-07-09 13:38:35 -0700

    Modifies git ignore to ignore bin and temp dir.

---
+++ 4_13/ex4-2.c
@@ -0,0 +1,16 @@
+/* 4_13/ex4-2.c
+ * write a program to print out a block E
+ */
+
+#include <stdio.h>
+
+int main (){
+  printf("*****\n");
+  printf("*\n");
+  printf("*\n");
+  printf("*****\n");
+  printf("*\n");
+  printf("*\n");
+  printf("*****\n");
+}
+
---
+++ 4_13/ex4-3.c
@@ -0,0 +1,21 @@
+/* 4_13/ex4-3.c
+ * calculate permiter of rectangle
+ */
+
+#include <stdio.h>
+
+int iwidth = 3;
+int iheight = 5;
+
+float fwidth = 6.8;
+float fheight = 2.3;
+
+int main (){
+
+  int ianswer = 2 * iwidth + 2 * iheight;
+  float fanswer = 2 * fwidth + 2 * fheight;
+
+  printf("Int perimeter: %d\n", ianswer);
+  printf("Float perimeter: %f\n", fanswer);
+
+}
